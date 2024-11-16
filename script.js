ab22cc5303a71be3b41959dc6f77f74d2bb0e6c2
Rhythm Doshi <rhythmdoshi31@Rhythms-MacBook-Air.local>
2024-11-16 16:23:10 +0530

    completed

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ tl.from("#line1-part1",{
             h1Timer.innerHTML = grow++;
             }
             else h1Timer.innerHTML = grow;
-        },20)
+        },10)
     }
 })
 tl.to(".line h2", {
@@ -57,14 +57,14 @@ tl.to(".line h2", {
 })
 tl.to("#loader",{
     opacity: 0,
-    duration: 0.3,
-    delay: 1.2,
+    duration: 0.25,
+    delay: 0.4,
 }) 
 
 tl.from("#page1", {
     y: 1600,
     opacity: 0,
-    duration: 0.7,
+    duration: 0.4,
     ease: Power4,
     backgroundColor: "#000000",
 })
@@ -79,21 +79,155 @@ tl.from("#nav, #page1 p, #page1>h6", {
 
 tl.from(".hero h1, #hero3 h2, h3", {
     y: 120,
-    stagger: 0.3,
+    stagger: 0.1,
 })
 tl.from("#page2", {
     opacity: 0,
 })
 }
-function crsr(){
-    document.addEventListener("mousemove", function(det){
-        gsap.to("#crsr", {
-            left: det.x,
-            top: det.y,
-        })     
+function sheryAnimation(){
+    Shery.imageEffect(".image-div", {
+        style: 5,
+        config:{"a":{"value":0.92,"range":[0,30]},"b":{"value":-0.85,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.8068921649330241},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.31,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.46,"range":[0,10]},"metaball":{"value":0.4,"range":[0,2]},"discard_threshold":{"value":0.57,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":6.87,"range":[0,100]}},
+        gooey: true,
     })
-    Shery.makeMagnet("#nav-part1 h4"); 
 }
+if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    function crsr(){
+        let left = 0, top= 0;
+        document.addEventListener("mousemove", function(det){
+            left = det.x;
+            top = det.y; 
+            updateCursor();    
+        }) 
+        scroll.on("scroll", (position) => {
+            updateCursor();
+        });
+        
+        function updateCursor() {
+            const transformY = scroll.scroll.instance.scroll.y || 0;  // Current scroll position from LocomotiveScroll
+            gsap.to("#crsr", {
+                x: left,
+                y: top + transformY,  // Adjust for the scroll position
+            });
+        }
+        Shery.makeMagnet("#nav-part1 h4");
+    }
+    crsr();
+}
+
+// Video-player code...
+let videoContainer = document.querySelector("#video-container");
+let video = document.querySelector("#video-container video");
+videoContainer.addEventListener("mouseenter", function(){
+    gsap.to("#crsr", {
+        opacity: 0,
+    });
+    videoContainer.addEventListener("mousemove", function(dets){
+        gsap.to("#video-cursor", {
+            left: dets.x - 480,
+            top: dets.y - 170,
+        })
+    });
+});
+videoContainer.addEventListener("mouseleave", function(){
+    gsap.to("#crsr", {
+        opacity: 1,
+    });
+    gsap.to("#video-cursor", {
+        left: "70%",
+        top: "-15%",
+    });
+})
+let flag = 0;
+videoContainer.addEventListener("click", function(){
+    if (flag == 0) {
+        video.play();
+        video.style.opacity = 1;
+        document.querySelector("#video-cursor").innerHTML = '<i class="ri-pause-fill"></i>';
+        gsap.to("#video-cursor", {
+        scale: 0.5,
+        })
+        flag = 1;
+    }
+    else {
+        video.pause();
+        video.style.opacity = 0;
+        document.querySelector("#video-cursor").innerHTML = '<i class="ri-play-large-fill"></i>';
+        gsap.to("#video-cursor", {
+        scale: 1,
+        })
+        flag = 0;
+    }
+})
+
+// Hero3 flag code...
+function  flagAnimation(){
+    let hero3 = document.querySelector("#hero3");
+document.addEventListener("mousemove", function(dets){
+    gsap.to("#flag", {
+        x: dets.x - 120,
+        y: dets.y,
+    })
+})
+hero3.addEventListener("mouseenter", function(){
+    gsap.to("#flag", {
+        opacity: 1,
+    })
+})
+hero3.addEventListener("mouseleave", function(){
+    gsap.to("#flag", {
+        opacity: 0,
+    })
+})
+}
+function footerAnimation() {
+
+    var clutter = ""
+    var clutter2 = ""
+    document.querySelector("#footer-text h1").textContent.split("").forEach(function (elem) {
+      clutter += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer-text h1").innerHTML = clutter
+    document.querySelector("#footer-text h2").textContent.split("").forEach(function (elem) {
+      clutter2 += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer-text h2").innerHTML = clutter2
+  
+  
+    document.querySelector("#footer-text").addEventListener("mouseenter", function () {
+      gsap.to("#footer-text h1 span", {
+        opacity: 0,
+        stagger: 0.05
+      })
+      gsap.to("#footer-text h2 span", {
+        delay: 0.25,
+        opacity: 1,
+        stagger: 0.05
+      })
+    })
+    document.querySelector("#footer-text").addEventListener("mouseleave", function () {
+      gsap.to("#footer-text h1 span", {
+        opacity: 1,
+        stagger: 0.05,
+        delay: 0.25,
+  
+      })
+      gsap.to("#footer-text h2 span", {
+        opacity: 0,
+        stagger: 0.05
+      })
+    })
+  }
+
+
+
 loader();
-crsr();
 locomotive();
+sheryAnimation();
+flagAnimation();
+footerAnimation();
